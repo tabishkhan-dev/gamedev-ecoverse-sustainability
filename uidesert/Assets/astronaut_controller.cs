38e4ef5e8d7b6cc84539d7f45d0aadf72a3a5c76
Aindrila <aindrila.acharya@tu-ilmenau.de>
2024-11-21 23:48:05 +0100

    movement to both characters

---
+++ uidesert/Assets/astronaut_controller.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class astronaut_controller : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetButtonDown("Jump")) // Spacebar for jump
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+}
+
+
