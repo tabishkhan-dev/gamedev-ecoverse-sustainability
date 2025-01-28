de9a06909beb74c750b5f87b3dca9a40bc4f9213
Riaz <md-riaz.khan@tu-ilmenau.de>
2025-01-28 02:19:21 +0100

    game menu, click, quiz and main level sounds added

--- uidesert/Assets/Scripts/GameplayManagerScripts/SceneLoader.cs
+++ uidesert/Assets/Scripts/GameplayManagerScripts/SceneLoader.cs
@@ -5,11 +5,25 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
+        // Play click sound
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlayButtonClickSound();
+        }
+
+        // Load the scene
         SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame()
     {
+        // Play click sound
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlayButtonClickSound();
+        }
+
+        // Exit the game
         Application.Quit();
     }
 }
